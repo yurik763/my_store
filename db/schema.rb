fda00dc55141cf241335f6ba4e2c3a650ff6e3c8
yurik763 <yurik763@tut.by>
2021-11-28 00:48:47 +0300

    add username for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_171242) do
+ActiveRecord::Schema.define(version: 2021_11_27_213531) do
 
   create_table "items", force: :cascade do |t|
     t.float "price"
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 2021_11_27_171242) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
