5564f4de5e801bac700156a4a71f97f1277bba57
yurik763 <yurik763@tut.by>
2021-11-30 19:46:13 +0300

    add migrate cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_213531) do
+ActiveRecord::Schema.define(version: 2021_11_30_162743) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.float "price"
