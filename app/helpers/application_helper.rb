049e4f4c83b7e292ef16bd5c1eac916b59565531
yurik763 <yurik763@tut.by>
2021-12-05 20:32:28 +0300

    add helper urls_to_images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def urls_to_images(s)
+		s.gsub! /\s(https:\/\/.*?)\s/,
+				'<p><img src="\1"/></p>'
+		s.html_safe
+	end
+
+	def urls_to_links(s)
+		s.gsub! /\s(https:\/\/.*?)\s/,
+				'<a href="\1">\1</a>'
+		s.html_safe
+	end
 end
